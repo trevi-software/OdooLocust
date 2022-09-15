fb8b25117ef80bd457a1b0858bc85626466cf4cc
Michael Telahun <mtm@trevi.et>
2022-09-15 11:13:17 +0300

    Modify OdooLocustUser to use OdooRPC instead of odoolib

--- OdooLocust/OdooLocustUser.py
+++ OdooLocust/OdooLocustUser.py
@@ -5,6 +5,7 @@
 # Copyright (C) 2011 Nicolas Vanhoren
 # Copyright (C) 2011 OpenERP s.a. (<http://openerp.com>).
 # Copyright (C) 2017 Nicolas Seinlet
+# Copyright (C) 2022 Michael Telahun Makonnen (<mtm@trevi.et>)
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
@@ -28,33 +29,35 @@
 # SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #
 ##############################################################################
-import odoolib
+from odoorpc import ODOO, error
 import time
 import sys
 
 from locust import HttpUser, events
 
 
-def send(self, service_name, method, *args):
-    if service_name == "object" and method == "execute_kw":
-        call_name = "%s : %s" % args[3:5]
-    else:
-        call_name = '%s : %s' % (service_name, method)
+def json(self, url, params):
     start_time = time.time()
+    call_name = url
+    if call_name == "/jsonrpc":
+        call_name = f"{params['args'][3]}:{params['args'][4]}"
+
     try:
-        res = odoolib.json_rpc(self.url, "call", {"service": service_name, "method": method, "args": args})
+        data = self._connector.proxy_json(url, params)
+        if data.get('error'):
+            raise error.RPCError(
+                data['error']['data']['message'], data['error']
+            )
     except Exception as e:
         total_time = int((time.time() - start_time) * 1000)
         events.request_failure.fire(request_type="Odoo JsonRPC", name=call_name, response_time=total_time, response_length=0, exception=e)
         raise e
-    else:
-        total_time = int((time.time() - start_time) * 1000)
-        events.request_success.fire(request_type="Odoo JsonRPC", name=call_name, response_time=total_time, response_length=sys.getsizeof(res))
-        return res
+    total_time = int((time.time() - start_time) * 1000)
+    events.request_success.fire(request_type="Odoo JsonRPC", name=call_name, response_time=total_time, response_length=sys.getsizeof(data))
+    return data
 
 
-odoolib.JsonRPCConnector.send = send
-odoolib.JsonRPCSConnector.send = send
+ODOO.json = json
 
 
 class OdooLocustUser(HttpUser):
@@ -65,16 +68,17 @@ class OdooLocustUser(HttpUser):
     password = "admin"
     protocol = "jsonrpc"
     user_id = -1
+    odoo_version = 0
 
     def on_start(self):
-        user_id = None
-        if self.user_id and self.user_id > 0:
-            user_id = self.user_id
-        self.client = odoolib.get_connection(hostname=self.host,
-                                             port=self.port,
-                                             database=self.database,
-                                             login=self.login,
-                                             password=self.password,
-                                             protocol=self.protocol,
-                                             user_id=user_id)
-        self.client.check_login(force=False)
+        # Prepare the connection to the server
+        odoo = ODOO(self.host, port=self.port, protocol=self.protocol)
+
+        # Login
+        odoo.login(self.database, self.login, self.password)
+
+        # Current user
+        self.user_id = odoo.env.user.id
+
+        self.client = odoo
+        self.odoo_version = odoo.version
